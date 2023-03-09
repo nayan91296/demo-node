a0f3c50aecfb19b5bc00b2695977a172a5a421a2
Nayan <nayan.v@elaunchinfotech.in>
2023-03-09 18:25:40 +0530

    getter

--- validation.js
+++ validation.js
@@ -1,11 +1,12 @@
 const response = require('./controllers/response');
 const Validator = require('validatorjs');
+const userModel = require('./models/user')
 
 function userRegister(req, res, next) {
 
   const validationRule = {
       "name": "required|string",
-      "email": "required|email",
+      "email": "required|email|exist:users,email",
       "password": "required|min:6|max:15",
   }
   validatorUtilWithCallback(validationRule, {}, req, res, next);
@@ -31,10 +32,34 @@ function userPost(req, res, next) {
 function validatorUtilWithCallback (rules, customMessages, req, res, next) {
   const validation = new Validator(req.body, rules, customMessages);
   validation.passes(() => next());
-  if(validation.fails()){
-    return res.status(400).send(response.validationError(formattedErrors(validation.errors.errors)));
-  }
+  validation.fails(() => res.status(400).send(response.validationError(formattedErrors(validation.errors.errors))));
+  // if(validation.fails()){
+  //   return res.status(400).send(response.validationError(formattedErrors(validation.errors.errors)));
+  // }
 };
+
+Validator.registerAsync('exist', function (value, attribute, req, passes) {
+  console.log('value',value,attribute);
+  if (!attribute) throw new Error('Specify Requirements i.e fieldName: exist:table,column');
+
+  let attArr = attribute.split(",");
+  if (attArr.length !== 2) throw new Error(`Invalid format for validation rule on ${attribute}`);
+  const { 0: table, 1: column } = attArr;
+
+  let msg = (column == "username") ? `${column} has already been taken ` : `${column} already in use`
+  var find_filed = {};
+  find_filed[column] = value;
+  // valueExists function for username, email, mobile number async function
+  userModel.findOne({ where: find_filed }).then((result) => {
+      if (result) {
+          passes(false, msg);
+      } else {
+          passes();
+      }
+  }).catch((err) => {
+      passes(false, err);
+  });
+});
 
 function formattedErrors(err) {
   let transformed = {};
